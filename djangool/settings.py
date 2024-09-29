886c1a0ccabc9c70bc5d851f49029561ec9be002
MLankaoui <marouanelankaoui66@gmail.com>
2024-09-29 17:22:39 +0100

    update the variable to be a constant.

--- djangool/settings.py
+++ djangool/settings.py
@@ -121,9 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-static_files = [
-        os.path.join(BASE_DIR, 'static/')
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
