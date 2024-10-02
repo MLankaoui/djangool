afbadec707055739023f82db80573c756b7c9120
MLankaoui <marouanelankaoui66@gmail.com>
2024-10-02 15:40:11 +0100

    finish the login feature pretty much.

--- app/views.py
+++ app/views.py
@@ -3,7 +3,7 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 
 
-def login_page(request):
+def login_page(request) -> render:
     if request.method == "POST":
         user_name = request.POST.get('username')
         password = request.POST.get('password')
@@ -13,12 +13,16 @@ def login_page(request):
         if user is not None:
             login(request, user)
             messages.success(request, f'{user_name} logged in successfully')
-            return redirect('home')
+            return redirect('loggedin')  # Corrected redirection
         else:
-            messages.warning(request, f'username or password incorrect')
+            messages.warning(request, 'Username or password incorrect')
     return render(request, 'app/login.html')
 
 
-def logout_user(request):
+def successfully_logged_in(request) -> render:  # Corrected function name
+    return render(request, 'app/successfully.html')  # Corrected template filename
+
+
+def logout_user(request) -> redirect:
     logout(request)
     return redirect('login')
--- landing/urls.py
+++ landing/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from landing.views import home
-from app.views import login_page
+from app.views import login_page, successfully_logged_in
 
 urlpatterns = [
     path ('', home, name="home"),
-    path('login/', login_page, name="login")
+    path('login/', login_page, name="login"),
+    path('logged_in/', successfully_logged_in, name="loggedin")
 ]
